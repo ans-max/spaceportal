2cba31c3bc6cee2139c8b85ae204ec401992fc69
anshuman mukherjee <anshuman.in@gmail.com>
2020-12-16 03:49:24 +0530

    Working Code, added basic logging

--- handlers.go
+++ handlers.go
@@ -1,45 +1,52 @@
 package main
 
 import (
-
-	"net/http"
+	"fmt"
 	"html/template"
-	"time"
 	"log"
-	//"fmt"
+	"net/http"
+	"time"
 )
 
 const (
-
 	layoutISO = "2006-01-02"
 	weblayout = "21/02/2006"
 )
 
-
 func ApodHandler(writer http.ResponseWriter, request *http.Request) {
 	var date string
 	currentTime := time.Now()
-	switch request.Method{
-	case "GET": {
-		date = currentTime.Format(layoutISO)
+	switch request.Method {
+	case "GET":
+		{
+			date = currentTime.Format(layoutISO)
 		}
-	case "POST": {
-		date = request.FormValue("date")
+	case "POST":
+		{
+			date = request.FormValue("date")
 		}
 	}
 	jresp := LookUpAPOD(date)
 	page := template.Must(template.ParseFiles("templates/apod.html"))
 	page.Execute(writer, jresp)
-	
 
+}
+
+func Log(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("%s:%s:%s:%s\n", r.RemoteAddr, r.UserAgent(), r.Method, r.URL.Path)
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func main() {
-
-	rh := http.RedirectHandler("https://www.ndtv.com", 307)
-	http.HandleFunc("/apod", ApodHandler)
+	mux := http.NewServeMux()
+	rh := http.RedirectHandler("https://timesofindia.indiatimes.com", 307)
+	mux.HandleFunc("/apod", ApodHandler)
+	mux.Handle("/news", rh)
 	log.Println("Listening in port 9090. . .")
-
-	http.ListenAndServe(":9090",nil)
+	err := http.ListenAndServe(":9090", Log(mux))
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
-
